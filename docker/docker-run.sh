056c11ab7223b68d166a0ef8bc2fe9e264440952
Daniel Gerblick <daniel.gerblick@gmail.com>
2020-10-02 13:17:28 -0400

    Added Dockerfile with zsh

---
+++ docker/docker-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WS_DIR="$(readlink -f $(dirname $0)/../../../)"
+echo "mounting host directory $WS_DIR as container directory /home/$USER/catkin_ws"
+
+docker run --rm -it \
+    -e DISPLAY \
+    -e LIBGL_ALWAYS_INDIRECT \
+    -v $XAUTH:/home/$USER/.Xauthority \
+    -v $WS_DIR:/home/$USER/catkin_ws \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --hostname arc-rocket-league-dev \
+    --name arc-rocket-league-dev \
+    arc-rocket-league-dev \
+    /bin/zsh
