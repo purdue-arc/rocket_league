a03ca1da4e68b2c1cc71a35d0ac8e1b0924ffc11
Harrison McCarty <mccarth@purdue.edu>
2020-11-14 13:44:05 -0500

    pr changes

--- racersim/src/racersim/sim.py
+++ racersim/src/racersim/sim.py
@@ -62,6 +62,7 @@ class Sim(object):
         self.car = Car(self.world, self.carDef)
         self.ball = Ball(self.world)
         self.goal = Goal(self.world)
+        self.path = None
 
         if renderEnabled:
             self.renderer = Renderer(bounds, scaling=scaling)
@@ -79,7 +80,11 @@ class Sim(object):
         if self.renderEnabled:
             self._render()
 
+    def render_path(self, path):
+        self.path = path
+
     def reset(self):
+        """Reset simulator to original state."""
         self.world.DestroyBody(self.car.body)
         self.world.DestroyBody(self.ball.body)
         self.world.DestroyBody(self.goal.body)
@@ -102,6 +107,7 @@ class Sim(object):
     def _render(self):
         """Render the current state of the sim."""
         try:
-            self.renderer.render(self.car, self.ball, self.goal, self.world)
+            self.renderer.render(self.car, self.ball, self.goal, \
+                                 self.world, path=self.path)
         except Renderer.ShutdownError:
             self.renderEnabled = False
