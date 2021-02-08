d81b5e97db2e43bb28abd4ca5f87f6b3fe54a911
Daniel Gerblick <daniel.gerblick@gmail.com>
2021-02-07 23:06:21 -0500

    Premature optimization

--- camera_tracking/src/camera_origin_node.cpp
+++ camera_tracking/src/camera_origin_node.cpp
@@ -12,17 +12,19 @@ int main(int argc, char* argv[]) {
 
   std::string cameraName;
   std::string originID;
+  double updateRate;
+
   ros::param::param<std::string>("~camera_name", cameraName, "camera");
   ros::param::param<std::string>("~origin_id", originID, "origin");
+  ros::param::param<double>("~update_rate", updateRate, 0.1);
 
   ros::Timer timer =
-      nh.createTimer(ros::Duration(1.0 / 20), [&](const ros::TimerEvent& e) {
+      nh.createTimer(ros::Duration(updateRate), [&](const ros::TimerEvent& e) {
         geometry_msgs::TransformStamped transform;
         try {
           transform = tfBuffer.lookupTransform(cameraName + "_" + originID,
                                                cameraName, ros::Time(0));
         } catch (tf2::TransformException e) {
-          // ROS_WARN_THROTTLE(60, "%s", e.what());
           return;
         }
         transform.header.frame_id = "map";
