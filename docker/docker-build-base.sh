16fe2cbe00d57f55b52092caa608d4cb136fb986
James Baxter <baxterjr@outlook.com>
2021-02-16 17:55:02 -0500

    typos and error handling

--- docker/docker-build-base.sh
+++ docker/docker-build-base.sh
@@ -5,6 +5,11 @@ REPO_NAME="purduearc/rocket-league"
 
 docker build -t $REPO_NAME $@ $DOCKER_DIR
 
++if [ $? -eq 0 ]; then
 echo "
 ARC Rocket League development image built as '$REPO_NAME'
 To push, run 'docker push $REPO_NAME:latest'"
+else
+echo "
+Error building image, see above messages" >&2
+fi
--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -13,9 +13,13 @@ docker build --build-arg USER=$USER \
              $@ \
              $DOCKER_DIR
 
++if [ $? -eq 0 ]; then
 echo "
 ARC Rocket League development image built as '$REPO_NAME:local'
 sudo password in container is 'robot'.
 Run 'sudo passwd' inside the container to change it,
 then run 'docker commit <container-id>' in a new terminal to make persistent"
-
+else
+echo "
+Error building image, see above messages" >&2
+fi
