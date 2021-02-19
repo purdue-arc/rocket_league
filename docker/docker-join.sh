6322a8593ed3747c66ce4de0a53fc74514338fa9
James Baxter <baxterjr@outlook.com>
2021-02-19 02:02:12 -0500

    hotfix to docker-join

--- docker/docker-join.sh
+++ docker/docker-join.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 CONTAINER_NAME="arc-rocket-league-dev"
-DOCKER_CMD=${DOCKER_CMD:+source /home/$USER/.zshrc && $DOCKER_CMD}
-
-echo /bin/zsh $DOCKER_CMD
 
 docker exec -it \
     -e DISPLAY \
     -e LIBGL_ALWAYS_INDIRECT \
     $@ \
     $CONTAINER_NAME \
-    $DOCKER_CMD
+    ${DOCKER_CMD:-/bin/zsh}
