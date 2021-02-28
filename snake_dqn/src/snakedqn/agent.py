bbab2c4b53669ada07765d4c0083a9804fd824b2
James Baxter <baxterjr@outlook.com>
2021-02-28 15:17:36 -0500

    fix training errors

--- snake_dqn/src/snakedqn/agent.py
+++ snake_dqn/src/snakedqn/agent.py
@@ -32,8 +32,6 @@ import torch
 from collections import deque
 import random
 
-import pdb
-
 class Agent(object):
     """High level DQN controller for snake tutorial."""
     def __init__(self, state_size, action_size, training=False):
@@ -67,20 +65,25 @@ class Agent(object):
         """Add experience to memory dequeue for retraining later."""
         self.memory.append((state, action, reward, next_state, done))
 
-    def _train(self, minibatch_size):
+    def _train(self, minibatch_size=100):
         """Update the model weights with past experiences from memory."""
         self.model.train()
+
+        # Check for sufficient data
+        if minibatch_size > len(self.memory):
+            print("Skipping training due to too few data")
+            return
 
         minibatch = random.sample(self.memory, minibatch_size)
         for state, action, reward, next_state, done in minibatch:
             if not done:
-                reward += self.gamma * np.amax(self.model(next_state))
+                reward += self.gamma * torch.max(self.model(next_state)).item()
 
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
 
             out = self.model(state)
 
-            target = out.copy()
+            target = out.detach().clone()
             target[action] = reward
 
             loss = self.loss(out, target)
@@ -88,26 +91,33 @@ class Agent(object):
 
             self.optimizer.step()
 
-    def train(self, step, reset):
+    def train(self, step, reset, episodes=1000):
         """Train the agent using provided step and reset functions."""
-        for episode in range(self.EPISODES):
+        for episode in range(episodes):
+            if episode % 25 == 0:
+                print(f"Beginning episode {episode} out of {episodes}")
+
             state, __, __, __ = reset()
+            state = self.transform_state(state)
             done = False
             while not done:
                 action = random.randint(0, self.action_size-1)
                 if random.random() > self.epsilon:
                     action = self.get_action(state)
                 next_state, reward, done, __ = step(action)
+                next_state = self.transform_state(next_state)
                 self.remember(state, action, reward, next_state, done)
-                
                 state = next_state
                 if self.epsilon > self.epsilon_min:
                     self.epsilon *= self.epsilon_decay
             self._train()
 
+    def transform_state(self, state):
+        """Transform state from numpy to torch type."""
+        return torch.from_numpy(state).to(self.device).float()
+
     def get_action(self, state):
         """Use forward propagation to get the predicted action from the model."""
-        state = torch.from_numpy(state).to(self.device).float()
         self.model.eval()
         reward = self.model(state)
         return torch.argmax(reward).item()
