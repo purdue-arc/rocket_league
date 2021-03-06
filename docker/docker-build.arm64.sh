eb7730b9fd80ce154593d6d18a75047be04886ad
James Baxter <baxterjr@outlook.com>
2021-03-06 18:08:40 -0500

    make seperate

---
+++ docker/docker-build.arm64.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DOCKER_DIR=$(readlink -f $(dirname $0))
+REPO_NAME="purduearc/rocket-league"
+
+docker build --build-arg USER=$USER \
+             --build-arg PW="robot" \
+             --build-arg UID=$(id -u) \
+             --build-arg GID=$(id -g) \
+             --build-arg ROS_DEPS="$ROS_DEPS" \
+             -t $REPO_NAME:local \
+             -f $DOCKER_DIR/Dockerfile.local \
+             $@ \
+             $DOCKER_DIR
+
+if [ $? -eq 0 ]; then
+echo "
+ARC Rocket League development image built as '$REPO_NAME:local'
+sudo password in container is 'robot'.
+Run 'sudo passwd' inside the container to change it,
+then run 'docker commit <container-id>' in a new terminal to make persistent"
+else
+echo "
+Error building image, see above messages" >&2
+fi
