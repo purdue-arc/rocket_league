6938d2772dd8682d5e00399d7beae872b9c94171
just007in <just007in2000@gmail.com>
2021-03-18 13:53:56 -0400

    Preliminary Support for PPM

--- hardware_interface/hardware_interface.ino
+++ hardware_interface/hardware_interface.ino
@@ -30,27 +30,30 @@
 
 #include <ros.h>
 #include <std_msgs/Float32.h>
+#include <PulsePositionIMXRT.h>
+#include <PulsePosition.h>
 
 // pins
-const byte THROTTLE_PIN = 9;
-const byte STEERING_PIN = 10;
+const byte PPM_OUT_PIN = 23;
 
-// limits
-const byte THROTTLE_MIN_PWM = 74;
-const byte THROTTLE_MID_PWM = 95;
-const byte THROTTLE_MAX_PWM = 109;
-
-const byte STEERING_MIN_PWM = 55;
-const byte STEERING_MID_PWM = 95;
-const byte STEERING_MAX_PWM = 131;
-
-// invert
-const float THROTTLE_SCALE = -1.0;
-const float STEERING_SCALE = -1.0;
+// channels
+const int THROTTLE_CHANNEL = 1;
+const int STEERING_CHANNEL = 2;
 
 
 ros::NodeHandle nh;
+PulsePositionOutput outputPPM(RISING);
 
+
+// Timing Parameters for PulsePosition Library
+// *********************DEFAULT VALUES******************
+// Minimum Time between Rising Edges: 300.0 microseconds
+// Maximum Time between Rising Edges: 3500.0 microseconds
+// *****************************************************
+const float effort_to_ppm_duration(const float effort)
+{
+  return effort;
+}
 const byte effort_to_pwm(const float effort, const byte MIN_PWN, const byte MID_PWM, const byte MAX_PWM)
 {
   if(effort >= 0)
@@ -65,12 +68,12 @@ const byte effort_to_pwm(const float effort, const byte MIN_PWN, const byte MID_
 
 void throttle_callback(const std_msgs::Float32& throttle)
 {
-  analogWrite(THROTTLE_PIN, effort_to_pwm(throttle.data * THROTTLE_SCALE, THROTTLE_MIN_PWM, THROTTLE_MID_PWM, THROTTLE_MAX_PWM));
+  outputPPM.write(THROTTLE_CHANNEL, effort_to_ppm_duration(throttle.data));
 }
 
 void steering_callback(const std_msgs::Float32& steering)
 {
-  analogWrite(STEERING_PIN, effort_to_pwm(steering.data * STEERING_SCALE, STEERING_MIN_PWM, STEERING_MID_PWM, STEERING_MAX_PWM));
+  outputPPM.write(STEERING_CHANNEL, effort_to_ppm_duration(steering.data));
 }
 
 ros::Subscriber<std_msgs::Float32> throttle_sub("control_effort/throttle", throttle_callback);
@@ -79,15 +82,16 @@ ros::Subscriber<std_msgs::Float32> steering_sub("control_effort/steering", steer
 void setup()
 {
   Serial.begin(57600);
-  pinMode(THROTTLE_PIN, OUTPUT); // analog
-  pinMode(STEERING_PIN, OUTPUT); // analog
-  analogWrite(THROTTLE_PIN, THROTTLE_MID_PWM);
-  analogWrite(STEERING_PIN, STEERING_MID_PWM);
-
+  pinMode(PPM_OUT_PIN, OUTPUT);
+  digitalWrite(PPM_OUT_PIN, LOW);
+  
   // ros
   nh.initNode();
   nh.subscribe(throttle_sub);
   nh.subscribe(steering_sub);
+
+  // begin sending PPM signal
+  outputPPM.begin(PPM_OUT_PIN);
 }
 
 void loop()
