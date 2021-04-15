4f2be5e42c190fab5e5e3397f358775327071f62
AJ Ernandes <aj.ernandes01@gmail.com>
2021-04-15 18:57:16 -0400

    switched from 4 to 2 cameras

--- rocket_league_estimation/src/BallDetection.cpp
+++ rocket_league_estimation/src/BallDetection.cpp
@@ -54,9 +54,9 @@ BallDetection::BallDetection() :
     pnh{"~"},
     image_transport{nh},
     posePub{nh.advertise<geometry_msgs::PoseWithCovarianceStamped>(
-        "ball_pose", 1)},
+        "ball_pose", 10)},
     camera_subscriber{image_transport.subscribeCamera(
-        "image_rect_color", 1, &BallDetection::BallCallback, this)},
+        "image_rect_color", 10, &BallDetection::BallCallback, this)},
     quad{pnh.param<int>("quad", 0)},
     originX{pnh.param<double>("originX", 0)},
     originY{pnh.param<double>("originY", 0)},
