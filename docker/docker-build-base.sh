a9dcfc7f141229ae78470128bc763ec23e7cb351
Daniel Gerblick <daniel.gerblick@gmail.com>
2021-09-14 13:26:30 -0400

    Removed platform specifics from Dockerfile

--- docker/docker-build-base.sh
+++ docker/docker-build-base.sh
@@ -6,8 +6,13 @@ REPO_NAME="purduearc/rocket-league-test-repository-please-ignore"
 ARCH=${ARCH:-"amd64, arm64"}
 PLATFORM_ARG=`printf '%s ' '--platform'; for var in $(echo $ARCH | sed "s/,/ /g"); do printf 'linux/%s,' "$var"; done | sed 's/,*$//g'`
 
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
 docker buildx build \
     ${PLATFORM_ARG} \
     --tag $REPO_NAME \
+    --cache-from=type=registry,ref=${REPO_NAME} \
+    --cache-to=type=registry,ref=${REPO_NAME},mode=max \
+    --push \
     $@ \
     $DOCKER_DIR
