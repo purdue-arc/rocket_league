305ad0a9c4062a33a2c4fdfe491296085dac6f10
Harrison McCarty <mccarth@purdue>
2021-10-27 13:20:19 -0400

    added full field configuration

--- rocket_league_simulation/src/simulator/sim.py
+++ rocket_league_simulation/src/simulator/sim.py
@@ -42,7 +42,7 @@ from simulator.car import Car
 class Sim(object):
     """Oversees components of the simulator"""
 
-    def __init__(self, ball_urdf_path, car_urdf_path, goal_urdf_path, goal_pos, render_enabled):
+    def __init__(self, urdf_paths, field_setup, render_enabled):
         if render_enabled:
             self._client = p.connect(p.GUI)
         else:
@@ -51,39 +51,46 @@ class Sim(object):
         p.setAdditionalSearchPath(p_data.getDataPath())
         self._planeID = p.loadURDF("plane.urdf")
 
+        zeroOrient = p.getQuaternionFromEuler([0, 0, 0])
         self._ballInitPos = [0, 0, 1]
-        self._ballInitOrient = p.getQuaternionFromEuler([0, 0, 0])
+        self._ballInitOrient = zeroOrient
         self._ballID = p.loadURDF(
-            ball_urdf_path, self._ballInitPos, self._ballInitOrient)
+            urdf_paths["ball"], self._ballInitPos, self._ballInitOrient)
 
-        self._goalPos = goal_pos
-        self._goalOrient = p.getQuaternionFromEuler([0, 0, 0])
-        self._goalWidth = 5.
-        self._goalHeight = 2.5
         self._goalID = p.loadURDF(
-            goal_urdf_path, self._goalPos, self._goalOrient, useFixedBase=1)
+            urdf_paths["goal"], field_setup["goal"], zeroOrient, useFixedBase=1)
+
+        p.loadURDF(
+            urdf_paths["sidewall"], field_setup["leftsidewall"], zeroOrient, useFixedBase=1)
+        p.loadURDF(
+            urdf_paths["sidewall"], field_setup["rightsidewall"], zeroOrient, useFixedBase=1)
+        p.loadURDF(
+            urdf_paths["backwall"], field_setup["frontbackwall"], zeroOrient, useFixedBase=1)
+        p.loadURDF(
+            urdf_paths["backwall"], field_setup["backbackwall"], zeroOrient, useFixedBase=1)
 
         self._cars = {}
         self._carID = p.loadURDF(
-            car_urdf_path, [0, 0, 0], p.getQuaternionFromEuler([0, 0, 0]))
-        self._cars[self._carID] = Car(self._carID, 0.5, [0.1, 0, 0], [0, 0, 0])
+            urdf_paths["car"], [0, 0, 0], zeroOrient))
+        self._cars[self._carID]=Car(
+            self._carID, 0.5, [0., -0.25, 0], [0, 0, math.pi/2])
 
-        self.touched_last = None
-        self.scored = False
-        self.running = True
+        self.touched_last=None
+        self.scored=False
+        self.running=True
 
         p.setGravity(0, 0, -10)
 
     def step(self, throttle_cmd, steering_cmd, dt):
         """Advance one time-step in the sim."""
         if self.running:
-            contacts = p.getContactPoints(bodyA=self._ballID)
+            contacts=p.getContactPoints(bodyA = self._ballID)
             for contact in contacts:
                 if contact[2] in self._cars:
-                    self.touchedLast = contact[2]
+                    self.touchedLast=contact[2]
                 elif contact[2] == self._goalID:
-                    self.scored = True
-                    self.running = False
+                    self.scored=True
+                    self.running=False
                     return
 
             for car in self._cars.values():
@@ -106,9 +113,9 @@ class Sim(object):
         return p.getBaseVelocity(self._ballID)
 
     def reset(self):
-        self.running = False
-        self.scored = False
-        self.touched_last = None
+        self.running=False
+        self.scored=False
+        self.touched_last=None
 
         p.resetBasePositionAndOrientation(
             self._ballID, self._ballInitPos, self._ballInitOrient)
@@ -116,4 +123,4 @@ class Sim(object):
         for car in self.cars:
             car.reset()
 
-        self.running = True
+        self.running=True
