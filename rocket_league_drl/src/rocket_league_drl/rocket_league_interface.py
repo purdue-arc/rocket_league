bd299dc20a8e040805032ce4a215af14f404d92f
James Baxter <baxterjr@outlook.com>
2021-11-03 13:46:54 -0400

    observations set up

--- rocket_league_drl/src/rocket_league_drl/rocket_league_interface.py
+++ rocket_league_drl/src/rocket_league_drl/rocket_league_interface.py
@@ -4,6 +4,7 @@ from rocket_league_drl import ROSInterface
 # ROS
 import rospy
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Int32
 from rocket_league_msgs.msg import Target
 from std_srv.srv import Empty
 
@@ -13,7 +14,7 @@ from gym.spaces import Box
 # System
 import numpy as np
 from tf.transformations import euler_from_quaternion
-from math import exp, pi
+from math import exp, pi, pow, sqrt
 
 class RocketLeagueInterface(ROSInterface):
     """ROS interface for the Rocket League."""
@@ -26,33 +27,40 @@ class RocketLeagueInterface(ROSInterface):
         self._MIN_TARGET_TIME = rospy.get_param('~target/time/min', 0.5)
         self._MAX_TARGET_TIME = rospy.get_param('~target/time/max', 5.0)
         self._MIN_TARGET_VEL = rospy.get_param('~target/velocity/min', 0.1)
-        self._MAX_TARGET_VEL = rospy.get_param('~target/velocity/max', 5.0)
-        self._MIN_TARGET_ANG_VEL = rospy.get_param('~target/angular_velocity/min', -pi)
-        self._MAX_TARGET_ANG_VEL = rospy.get_param('~target/angular_velocity/max', pi)
-
+        self._MAX_TARGET_VEL = rospy.get_param('~target/velocity/max', 1.5)
+        self._MAX_TARGET_ANG_VEL = rospy.get_param('~target/angular_velocity/max_abs', pi)
+        self._MAX_OBS_VEL = rospy.get_param('~observation/velocity/max_abs', 3.0)
+        self._MAX_OBS_ANG_VEL = rospy.get_param('~observation/angular_velocity/max_abs', 2*pi)
+        self._MAX_TIME = rospy.get_param('~max_episode_time', 30.0)
 
         # Publishers
         self._action_pub = rospy.Publisher('self/target', Target, queue_size=1)
         self._reset_srv = rospy.ServiceProxy('sim/reset', Empty)
 
         # State variables
-        self._self_odom = None
+        self._car_odom = None
         self._ball_odom = None
         self._score = None
         self._prev_time = None
         self._prev_score = None
+        self._start_time = None
 
         # Subscribers
-        rospy.Subscriber('self/odom', Odometry, self._self_odom_cb)
+        rospy.Subscriber('self/odom', Odometry, self._car_odom_cb)
         rospy.Subscriber('ball/odom', Odometry, self._ball_odom_cb)
+        rospy.Subscriber('game/score', Int32, self._score_cb)
 
     @property
     def action_size(self):
         """The Space object corresponding to valid actions."""
         return Box(
             # delta T, x, y, theta, v, omega
-            low =np.array(self._MIN_TARGET_TIME, -self._FIELD_WIDTH/2, -self._FIELD_HEIGHT/2, -pi, self._MIN_TARGET_VEL, -self._MIN_TARGET_ANG_VEL),
-            high=np.array(self._MAX_TARGET_TIME,  self._FIELD_WIDTH/2,  self._FIELD_HEIGHT/2,  pi, self._MAX_TARGET_VEL,  self._MAX_TARGET_ANG_VEL),
+            low =np.array(self._MIN_TARGET_TIME,
+                -self._FIELD_WIDTH/2, -self._FIELD_HEIGHT/2,
+                -pi, self._MIN_TARGET_VEL, -self._MAX_TARGET_ANG_VEL),
+            high=np.array(self._MAX_TARGET_TIME,
+                self._FIELD_WIDTH/2, self._FIELD_HEIGHT/2,
+                pi, self._MAX_TARGET_VEL, self._MAX_TARGET_ANG_VEL),
             dtype=np.float32)
 
     @property
@@ -60,8 +68,18 @@ class RocketLeagueInterface(ROSInterface):
         """The Space object corresponding to valid observations."""
         locations = 2*(1+self._NUM_SEGMENTS)
         return Box(
-            low=np.array([-pi] + locations*[0]),
-            high=np.array([pi] + locations*[self._FIELD_SIZE]),
+            # x, y, theta, v, omega (car)
+            # x, y, vx, vy (ball)
+            low=np.array(
+                -self._FIELD_WIDTH/2, -self._FIELD_HEIGHT/2,
+                -pi, -self._MAX_OBS_VEL, -self._MAX_OBS_ANG_VEL,
+                -self._FIELD_WIDTH/2, -self._FIELD_HEIGHT/2,
+                -self._MAX_OBS_VEL, -self._MAX_OBS_VEL),
+            high=np.array(
+                self._FIELD_WIDTH/2, self._FIELD_HEIGHT/2,
+                pi, self._MAX_OBS_VEL, self._MAX_OBS_ANG_VEL,
+                self._FIELD_WIDTH/2, self._FIELD_HEIGHT/2,
+                self._MAX_OBS_VEL, self._MAX_OBS_VEL),
             dtype=np.float32)
 
     def _reset_env(self):
@@ -73,6 +91,9 @@ class RocketLeagueInterface(ROSInterface):
         self.clear_state()
         self._prev_time = None
         self._prev_score = None
+        self._start_time = None
+
+        #@TODO log data
 
     def _has_state(self):
         """Determine if the new state is ready."""
@@ -93,29 +114,30 @@ class RocketLeagueInterface(ROSInterface):
         """Get state tuple (observation, reward, done, info)."""
         assert self.has_state()
 
-        # combine pose / goal for observation
-        pose = np.asarray(self._pose, dtype=np.float32)
-        goal = np.asarray(self._goal, dtype=np.float32)
-        observation = np.concatenate((pose, goal))
+        # combine car / ball odomes for observation
+        car = np.asarray(self._car_odom, dtype=np.float32)
+        ball = np.asarray(self._ball_odom, dtype=np.float32)
+        observation = np.concatenate((car, ball))
 
-        # Determine reward and if done
+        # Determine reward
         reward = 0
-        done = False
 
         time = rospy.Time.now()
-        if self._prev_time is not None:
-            dist = np.sqrt(np.sum(np.square(pose[1:3] - goal)))
-            dist_reward_rate = self._BASE_REWARD * exp(-1.0 * self._EXP_REWARD * dist)
-            reward += (time - self._prev_time).to_sec() * dist_reward_rate
+        # @TODO reward
+        # if self._prev_time is not None:
+        #     dist = np.sqrt(np.sum(np.square(pose[1:3] - goal)))
+        #     dist_reward_rate = self._BASE_REWARD * exp(-1.0 * self._EXP_REWARD * dist)
+        #     reward += (time - self._prev_time).to_sec() * dist_reward_rate
         self._prev_time = time
 
         if self._prev_score is not None:
             reward += self._GOAL_REWARD * (self._score - self._prev_score)
         self._prev_score = self._score
 
-        if not self._alive:
-            reward += self._DEATH_REWARD
-            done = True
+        # check if time exceeded
+        if self._start_time is None:
+            self._start_time = time
+        done = (time - self._start_time).to_sec() >= self._MAX_TIME
 
         return (observation, reward, done, {})
 
@@ -132,39 +154,44 @@ class RocketLeagueInterface(ROSInterface):
 
         self._action_pub.publish(action_msg)
 
-    def _pose_cb(self, pose_msg):
-        """Callback for poses of each segment of snake."""
-        assert len(pose_msg.poses) == self._NUM_SEGMENTS
-
+    def _car_odom_cb(self, odom_msg):
+        """Callback for odometry of car."""
+        x = odom_msg.pose.pose.position.x
+        y = odom_msg.pose.pose.position.y
         yaw, __, __ = euler_from_quaternion((
-            pose_msg.poses[0].orientation.x,
-            pose_msg.poses[0].orientation.y,
-            pose_msg.poses[0].orientation.z,
-            pose_msg.poses[0].orientation.w))
+            odom_msg.pose.pose.orientation.x,
+            odom_msg.pose.pose.orientation.y,
+            odom_msg.pose.pose.orientation.z,
+            odom_msg.pose.pose.orientation.w))
+        vx = odom_msg.twist.twist.linear.x
+        vy = odom_msg.twist.twist.linear.y
+        v = sqrt(pow(vx, 2) + pow(vy, 2))
+        omega = odom_msg.twist.twist.angular.z
 
-        self._pose = tuple(
-            [yaw] +
-            [func(pose_msg.poses[i]) for i in range(self._NUM_SEGMENTS)
-                for func in (
-                    lambda pose: pose.position.x,
-                    lambda pose: pose.position.y)])
+        # @TODO bounds checking?
+        self._car_odom = (
+            x, y, yaw,
+            v, omega)
+
         with self._cond:
             self._cond.notify_all()
 
-    def _goal_cb(self, goal_msg):
-        """Callback for location of goal."""
-        self._goal = (goal_msg.point.x, goal_msg.point.y)
+    def _ball_odom_cb(self, odom_msg):
+        """Callback for odometry of ball."""
+        x = odom_msg.pose.pose.position.x
+        y = odom_msg.pose.pose.position.y
+        vx = odom_msg.twist.twist.linear.x
+        vy = odom_msg.twist.twist.linear.y
+
+        # @TODO bounds checking?
+        self._ball_odom = (
+            x, y, vx, vy)
+
         with self._cond:
             self._cond.notify_all()
 
     def _score_cb(self, score_msg):
         """Callback for score of game."""
         self._score = score_msg.data
-        with self._cond:
-            self._cond.notify_all()
-
-    def _alive_cb(self, alive_msg):
-        """Callback for active state of snake."""
-        self._alive = alive_msg.data
         with self._cond:
             self._cond.notify_all()
