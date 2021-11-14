3c71c2cfda52370c2d53889ebe7cd4b2cdd93a72
James Baxter <baxterjr@outlook.com>
2021-11-14 15:49:52 -0500

    possible fix

--- rocket_league_control/scripts/ros.m
+++ rocket_league_control/scripts/ros.m
@@ -1,8 +1,21 @@
-% rosinit("192.168.11.119");
+try
+    setenv('ROS_MASTER_URI','http://192.168.11.119:11311')
+    setenv('ROS_IP','192.168.11.116')
+    rosinit
+catch err
+end
 
-sub = rossubscriber('car0/pose','DataFormat','struct');
+callback("fake");
+
+sub = rossubscriber('/car0/pose', @callback);
 pause(1);
 
-while true
-    [msg2,status,statustext] = receive(sub);
+% while true
+% [msg2,status,statustext] = receive(sub, 1);
+% end
+
+% rosshutdown
+
+function callback(msg)
+msg
 end
