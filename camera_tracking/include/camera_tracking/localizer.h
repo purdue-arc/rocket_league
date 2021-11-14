6743ad01516c1d4497485251b84559238c902c2a
Daniel Gerblick <daniel.gerblick@gmail.com>
2021-11-14 02:25:48 -0500

    Maybe data got lost in translation, maybe I filtered too much, And maybe tf was a masterpiece, 'til Kabsch tore it up Running scared, I was there, I coded it all too well

--- camera_tracking/include/camera_tracking/localizer.h
+++ camera_tracking/include/camera_tracking/localizer.h
@@ -3,26 +3,33 @@
 
 #include <ros/ros.h>
 #include <apriltag_ros/AprilTagDetectionArray.h>
+#include <Eigen/Dense>
 
 namespace camera_tracking {
 
 class Localizer {
 public:
   Localizer(const ros::NodeHandle& nh, const std::string& detectionTopic,
-            const std::string& originId,
-            const std::map<std::string, std::string>& pubTopics, int bufferSize,
-            int queueSize);
+            const std::string& originId, const std::string& pubTopic,
+            const std::map<std::string, std::string>& pubTopics, int bufferSize, int queueSize);
   ~Localizer() = default;
 
   static std::string idsToString(std::vector<int> ids);
 
 private:
   void callback(apriltag_ros::AprilTagDetectionArrayConstPtr msg);
+  Eigen::Matrix4d combineMatrices(const Eigen::Matrix3d& rot, const Eigen::Vector3d& pos);
+  geometry_msgs::PoseWithCovarianceStamped toMsg(const Eigen::Matrix4d& transform, ros::Time stamp,
+                                                 const std::string& frameId = "map");
 
   ros::NodeHandle _nh;
   ros::Subscriber _sub;
-  std::map<std::string, ros::Publisher> _pubs;
   std::string _originId;
+  ros::Publisher _pub;
+  std::map<std::string, ros::Publisher> _pubs;
+  int _bufferSize;
+  int _bufferPos;
+  std::vector<std::pair<Eigen::Matrix3d, Eigen::Vector3d>> _buffer;
 };
 
 }  // namespace camera_tracking
--- camera_tracking/src/localizer_node.cpp
+++ camera_tracking/src/localizer_node.cpp
@@ -6,18 +6,19 @@ int main(int argc, char* argv[]) {
 
   std::string detectionTopic;
   std::string originId;
+  std::string pubTopic;
   std::map<std::string, std::string> pubTopics;
   int bufferSize;
   int queueSize;
 
-  ros::param::param<std::string>("~dectection_topic", detectionTopic,
-                                 "tag_detections");
+  ros::param::param<std::string>("~dectection_topic", detectionTopic, "tag_detections");
   ros::param::param<std::string>("~origin_id", originId, "0");
+  ros::param::get("~pub_topic", pubTopic);
   ros::param::get("~pub_topics", pubTopics);
   ros::param::param<int>("~buffer_size", bufferSize, 10);
   ros::param::param<int>("~queue_size", queueSize, 100);
 
-  camera_tracking::Localizer localizer(nh, detectionTopic, originId, pubTopics,
+  camera_tracking::Localizer localizer(nh, detectionTopic, originId, pubTopic, pubTopics,
                                        bufferSize, queueSize);
 
   ros::spin();
