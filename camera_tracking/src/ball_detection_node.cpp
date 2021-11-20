2432075043e036a9258a2828ee17f245ff8fc22e
ajernandes <aj.ernandes01@gmail.com>
2021-11-20 11:47:13 -0500

    Slight refactor - Comments, imports, & rearranging

--- camera_tracking/src/ball_detection_node.cpp
+++ camera_tracking/src/ball_detection_node.cpp
@@ -25,27 +25,6 @@
 *******************************************************************************/
 
 #include "camera_tracking/ball_detection_node.h"
-//#include "rocket_league_estimation/PHCModel.h"
-
-
-//ros stuff
-//#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <geometry_msgs/Vector3Stamped.h>
-#include <image_transport/camera_subscriber.h>
-
-//open cv stuff
-#include <opencv2/opencv.hpp> //open cv core
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp> //opencv window stuff
-#include <opencv2/core/types.hpp> //convert pixel to irl cords
-#include <cv_bridge/cv_bridge.h> //convert ros to open cv
-#include <image_geometry/pinhole_camera_model.h>
-//cpp includes
-#include <iostream>
-#include <string> 
-#include <vector>
-#include <math.h>
 
 int getMaxAreaContourId(std::vector <std::vector<cv::Point>> contours);
 
@@ -71,76 +50,82 @@ BallDetection::BallDetection() :
     {}
 
 void BallDetection::BallCallback(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& info) {
-    cv_bridge::CvImagePtr cv_ptr;
     try {
-        //define published messages
+        /* define published vector */
         geometry_msgs::Vector3Stamped vec;
-        sensor_msgs::Image threshImg;
-        // Convert the ROS message  
-        cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
-        // Store the values of the OpenCV-compatible image
-        // into the current_frame variable
+
+        /* Convert the ROS message into a cv_ptr & dereferencing the pointer to an OpenCV Mat */
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
         cv::Mat current_frame = cv_ptr->image;
         cv::Mat frame_HSV, frame_threshold;
-        //resize image
+        
+        #if false /* shouldn't be necessary on the new computers */
+        /* resize image for performance */
         cv::resize(current_frame, current_frame, cv::Size(), 0.5, 0.5);
-        // Convert from BGR to HSV colorspace
+        #endif
+
+        /* Convert from BGR to HSV colorspace */
         cvtColor(current_frame, frame_HSV, cv::COLOR_BGR2HSV);
-        //get image size
+        
+        /* get image size */
         int h = current_frame.size().height;
         int w = current_frame.size().width;
-        // Detect the object based on HSV Range Values
+
+        /* Detect the object based on HSV Range Values */
         inRange(frame_HSV, cv::Scalar(minHue, minSat, minVib), cv::Scalar(maxHue, maxSat, maxVib), frame_threshold);
         erode(frame_threshold, frame_threshold, cv::Mat(), cv::Point(-1, -1), 2, 1, 1);
         dilate(frame_threshold, frame_threshold, cv::Mat(), cv::Point(-1, -1), 2, 1, 1);
-        //find the centers
+
+        /* find all the contours */
         std::vector<std::vector<cv::Point> > contours;
         std::vector<cv::Vec4i> hierarchy;
         findContours(frame_threshold, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+        /* only find the center if there are detections */
         if (contours.size() != 0) {
-            //find largest contour
+
+            /* find largest contour */
             cv::Moments moment = cv::moments(contours.at(getMaxAreaContourId(contours)));
-            //calculates the center
+
+            /* calculates the center of the contour*/
             double centerX = moment.m10 / moment.m00;
             double centerY = moment.m01 / moment.m00;
-
             double centerdX = (w/2) - centerX;
             double centerdY = (h/2) - centerY;
 
-            //create camera model
+            /* create camera model and project the pixel to a vector */
             camera.fromCameraInfo(info);
             cv::Point3d cam = camera.projectPixelTo3dRay(cv::Point2d(centerX, centerY));
             
-            //create the vector to publish
+            /* create the vector to publish */
             vec.vector.x = cam.x;
             vec.vector.y = cam.y;
             vec.vector.z = cam.z;
             
-            if (publishThresh) {
-                
-                /* publishes the threshold image */
-
-                std_msgs::Header header;
-                cv_bridge::CvImage img_bridge;
-                sensor_msgs::Image img_msg;
-                header.stamp = ros::Time::now(); 
-                img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, frame_threshold);
-                img_bridge.toImageMsg(img_msg);
-                imgPub.publish(img_msg);     
-            }
-            
+            /* publish the location vector */
             vec.header = msg->header;
             vecPub.publish(vec);
+
+            /* publishes the threshold image */
+            if (publishThresh) {
+                sensor_msgs::Image threshImg;
+                std_msgs::Header header;
+                cv_bridge::CvImage img_bridge;
+                header.stamp = ros::Time::now(); 
+                img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, frame_threshold);
+                img_bridge.toImageMsg(threshImg);
+                imgPub.publish(threshImg);     
+            }
         }
     }
     catch (cv_bridge::Exception& e) {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+        ROS_ERROR("There was some error, likely with image conversion");
     }
 }
 
 int getMaxAreaContourId(std::vector <std::vector<cv::Point>> contours) {
+    /* calculates the area of each contour and then returns the largest one */
     double maxArea = 0;
-    //calculates the area of each contour and then returns the largest one.
     int maxAreaContourId = -1;
     for (int j = 0; j < contours.size(); j++) {
         double newArea = cv::contourArea(contours.at(j));
