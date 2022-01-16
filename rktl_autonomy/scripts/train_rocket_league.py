f9eb7e1a60189b7acda7ada58b06ddc403429d3e
James Baxter <baxterjr@outlook.com>
2022-01-16 17:14:22 -0500

    changes for rocket league

--- rktl_autonomy/scripts/train_rocket_league.py
+++ rktl_autonomy/scripts/train_rocket_league.py
@@ -18,7 +18,7 @@ model = PPO("MlpPolicy", env)
 
 # log training progress as CSV
 log_dir = normpath(expanduser(rospy.get_param('~log/base_dir'))) + "/" + env.get_run_uuid()
-logger = configure(log_dir, ["stdout", "csv"])
+logger = configure(log_dir, ["stdout", "csv", "log"])
 model.set_logger(logger)
 
 # log model weights
