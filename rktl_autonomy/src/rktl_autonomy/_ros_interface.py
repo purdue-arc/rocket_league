7271ed2967fc28464c261664165c18cbf854508a
James Baxter <baxterjr@outlook.com>
2022-02-01 09:25:46 -0500

    render only first env

--- rktl_autonomy/src/rktl_autonomy/_ros_interface.py
+++ rktl_autonomy/src/rktl_autonomy/_ros_interface.py
@@ -74,7 +74,7 @@ class ROSInterface(Env):
             ros_id = roslaunch.rlutil.get_or_generate_uuid(None, False)
             roslaunch.configure_logging(ros_id)
             launch_file = roslaunch.rlutil.resolve_launch_arguments(launch_file)[0]
-            launch_args += [f'agent_name:={node_name}', f'plot_log:={self.__LOG}']
+            launch_args += [f'agent_name:={node_name}', f'plot_log:={self.__LOG}', f'render:={port==11311}']
             launch = roslaunch.parent.ROSLaunchParent(ros_id, [(launch_file, launch_args)], port=port)
             launch.start()
             # initialize self
