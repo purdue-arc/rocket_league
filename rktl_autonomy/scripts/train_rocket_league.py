5a3d48d012643a6a1e66f2c52506e99e55f22ff3
James Baxter <baxterjr@outlook.com>
2022-02-01 09:22:47 -0500

    prevent zombies

--- rktl_autonomy/scripts/train_rocket_league.py
+++ rktl_autonomy/scripts/train_rocket_league.py
@@ -15,28 +15,30 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 from os.path import expanduser
 import uuid
 
-run_id = str(uuid.uuid4())
+if __name__ == '__main__':
+    run_id = str(uuid.uuid4())
 
-# env = RocketLeagueInterface(run_id=run_id)
-env = make_vec_env(RocketLeagueInterface, env_kwargs={'run_id':run_id}, n_envs=4,
-        vec_env_cls=SubprocVecEnv, vec_env_kwargs={'start_method':'fork'})
+    # env = RocketLeagueInterface(run_id=run_id)
+    env = make_vec_env(RocketLeagueInterface, env_kwargs={'run_id':run_id},
+            n_envs=4, vec_env_cls=SubprocVecEnv)
 
-model = PPO("MlpPolicy", env)
+    model = PPO("MlpPolicy", env)
 
-# log training progress as CSV
-log_dir = expanduser(f'~/catkin_ws/data/rocket_league/{run_id}')
-logger = configure(log_dir, ["stdout", "csv", "log"])
-model.set_logger(logger)
+    # log training progress as CSV
+    log_dir = expanduser(f'~/catkin_ws/data/rocket_league/{run_id}')
+    logger = configure(log_dir, ["stdout", "csv", "log"])
+    model.set_logger(logger)
 
-# log model weights
-freq = 25000
-callback = CheckpointCallback(save_freq=freq, save_path=log_dir)
+    # log model weights
+    freq = 25000
+    callback = CheckpointCallback(save_freq=freq, save_path=log_dir)
 
-# run training
-steps = 100000
-print(f"training on {steps} steps")
-model.learn(total_timesteps=steps, callback=callback)
+    # run training
+    steps = 10000
+    print(f"training on {steps} steps")
+    model.learn(total_timesteps=steps, callback=callback)
 
-# save final weights
-print("done training")
-model.save(log_dir + "/final_weights")
+    # save final weights
+    print("done training")
+    model.save(log_dir + "/final_weights")
+    env.close()
