9afd92c28e909848c8f77268611f3916f5ec2a08
James Baxter <baxterjr@outlook.com>
2022-02-15 14:52:53 -0500

    better process noise, 10 Hz control inputs

--- rktl_control/scripts/kalman.m
+++ rktl_control/scripts/kalman.m
@@ -16,14 +16,16 @@ ORIGIN_DIR_STD_DEV = deg2rad(10);   % uncertainty of initial heading, rad
 MEAS_LOC_STD_DEV = 0.05;            % uncertainty of location measurement, m
 MEAS_DIR_STD_DEV = deg2rad(5);      % uncertainty of heading measurment, rad
 
+PROC_VEL_STD_DEV = 0.025;           % process noise for velocity, m/s
+PROC_BETA_STD_DEV = deg2rad(0.75);  % process noise for beta, rad
 
 % physical properties of car
 global CAR_LENGTH MAX_STEERING STEERING_RATE MAX_SPEED SPEED_RATE;
-CAR_LENGTH = .01;              % wheel to wheel dist, m
-MAX_STEERING = deg2rad(15);    % center-side throw, rad
-STEERING_RATE = deg2rad(5);    % ROC steering, rad per sec
-MAX_SPEED = 1;                 % max speed, m/s
-SPEED_RATE = 0.1;              % ROC speed, m/s2
+CAR_LENGTH = .01;               % wheel to wheel dist, m
+MAX_STEERING = deg2rad(15);     % center-side throw, rad
+STEERING_RATE = deg2rad(30);    % ROC steering, rad per sec
+MAX_SPEED = 1;                  % max speed, m/s
+SPEED_RATE = 1;                 % ROC speed, m/s2
 
 %% Generate randomized ground truth data
 % pre-allocate matrix to hold output
@@ -35,14 +37,19 @@ ground_truth = zeros(5, DURATION/TRUTH_DELTA_T);
 % random position, zero velocity, centered steering
 state = randn(5, 1).*[ORIGIN_LOC_STD_DEV; ORIGIN_LOC_STD_DEV; ORIGIN_DIR_STD_DEV; 0; 0];
 
+% generate an initial control (throttle; steering)
+% throttle uniform between 0.5 and 1.0
+% steering uniform between -1.0 and 1.0
+control = rand(2, 1).*[0.5; 2] + [0.5; -1];
+
 for i = 1:size(ground_truth,2)
     % add to raw_data
     ground_truth(:,i) = state;
 
-    % generate random controls
-    % throttle uniform between 0.5 and 1.0
-    % steering uniform between -1.0 and 1.0
-    control = rand(2, 1).*[0.5; 2] + [0.5; -1];
+    % update random control with same frequency as filter
+    if (mod(i, FILTER_DELTA_T/TRUTH_DELTA_T) == 0)
+        control = rand(2, 1).*[0.5; 2] + [0.5; -1];
+    end
 
     % step
     state = simulate(state, control, TRUTH_DELTA_T);
@@ -57,9 +64,9 @@ measurements = ground_truth(1:3, 1:(FILTER_DELTA_T/TRUTH_DELTA_T):end);
 measurements = measurements + randn(size(measurements)).*[MEAS_LOC_STD_DEV; MEAS_LOC_STD_DEV; MEAS_DIR_STD_DEV];
 
 %% Paramters for EKF
-% @TODO process noise
-% this is a rough guess and should be formally calculated at some point
-Q = eye(5).*[0.001; 0.001; deg2rad(1); 0.1; deg2rad(3)].^2;
+% process noise
+% project v, beta error onto model later
+Q0 = eye(5).*[0; 0; 0; PROC_VEL_STD_DEV; PROC_BETA_STD_DEV].^2;
 
 % observation matrix (can only see position, not internal state)
 H = [eye(3), zeros(3,2)];
@@ -90,6 +97,7 @@ for i = 1:size(measurements,2)
 
     % Extrapolate
     [next_state, F] = extrapolate(state, FILTER_DELTA_T);
+    Q = F*Q0*F';
     next_cov = F*cov*F'+ Q;
 
     % Kalman gain
@@ -102,20 +110,42 @@ end
 
 %% Graphical output
 figure
-% plot ground truth (no noise)
+subplot(2,2,1)
 plot(ground_truth(1,:), ground_truth(2,:))
 hold, grid on
-
-% plot measurements (with noise)
 plot(measurements(1,:), measurements(2,:), 'x')
-
-% plot filter output
 plot(estimates(1,:), estimates(2,:), '--')
-
-% plot origin
 plot(ground_truth(1,1), ground_truth(2,1), '*b')
-
 legend(["Ground Truth", "Measurements", "Estimates"])
+title("XY tracking")
+
+subplot(2,2,2)
+i = 1;
+plot(ground_truth(i, 1:(FILTER_DELTA_T/TRUTH_DELTA_T):end))
+hold, grid on
+plot(measurements(i,:), 'x')
+plot(estimates(i,:), '--')
+legend(["Ground Truth", "Measurements", "Estimates"])
+title("X tracking")
+
+subplot(2,2,3)
+i = 2;
+plot(ground_truth(i, 1:(FILTER_DELTA_T/TRUTH_DELTA_T):end))
+hold, grid on
+plot(measurements(i,:), 'x')
+plot(estimates(i,:), '--')
+legend(["Ground Truth", "Measurements", "Estimates"])
+title("Y tracking")
+
+subplot(2,2,4)
+i = 3;
+plot(ground_truth(i, 1:(FILTER_DELTA_T/TRUTH_DELTA_T):end))
+hold, grid on
+plot(measurements(i,:), 'x')
+plot(estimates(i,:), '--')
+legend(["Ground Truth", "Measurements", "Estimates"])
+title("Theta tracking")
+
 
 %% Helper functions
 function [next_state, F] = extrapolate(state, DELTA_T)
@@ -136,10 +166,10 @@ function [next_state, F] = extrapolate(state, DELTA_T)
 
     % calculate jacobian (linearization of this function about this point)
     F = [1, 0, -DELTA_T*v*sin(beta + theta),        DELTA_T*cos(beta + theta),       -DELTA_T*v*sin(beta + theta);
-        0, 1,  DELTA_T*v*cos(beta + theta),        DELTA_T*sin(beta + theta),        DELTA_T*v*cos(beta + theta);
-        0, 0,                            1, (2*DELTA_T*sin(beta))/CAR_LENGTH, (2*DELTA_T*v*cos(beta))/CAR_LENGTH;
-        0, 0,                            0,                                1,                                  0;
-        0, 0,                            0,                                0,                                  1];
+         0, 1,  DELTA_T*v*cos(beta + theta),        DELTA_T*sin(beta + theta),        DELTA_T*v*cos(beta + theta);
+         0, 0,                            1, (2*DELTA_T*sin(beta))/CAR_LENGTH, (2*DELTA_T*v*cos(beta))/CAR_LENGTH;
+         0, 0,                            0,                                1,                                  0;
+         0, 0,                            0,                                0,                                  1];
 end
 
 function next_state = simulate(state, control, DELTA_T)
