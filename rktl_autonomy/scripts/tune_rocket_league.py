6e0ee23c6afd85afbcd3e79aab87e12467c531e6
Haddy Alchaer <halchaer@purdue.edu>
2022-03-03 03:11:22 +0000

    updated tuning script

--- rktl_autonomy/scripts/tune_rocket_league.py
+++ rktl_autonomy/scripts/tune_rocket_league.py
@@ -21,30 +21,39 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 from os.path import expanduser
 import uuid
 import optuna
+import sys
+
+from stable_baselines3.common.vec_env import VecCheckNan
+
 
 run_id = str(uuid.uuid4())  # ALL running environments must share this
 step = 0
+study = optuna.create_study()
 
 def optimize_ppo2(trial):
     """ Learning hyperparamters we want to optimise"""
     return {
         'n_steps': int(trial.suggest_loguniform('n_steps', 16, 2048)),
-        'gamma': trial.suggest_loguniform('gamma', 0.9, 0.9999),
-        'learning_rate': trial.suggest_loguniform('learning_rate', 1e-5, 1.),
-        'ent_coef': trial.suggest_loguniform('ent_coef', 1e-8, 1e-1),
-        'clip_range': trial.suggest_uniform('clip_range', 0.1, 0.4),
-        # 'noptepochs': int(trial.suggest_loguniform('noptepochs', 1, 48)),
-        'gae_lambda': trial.suggest_uniform('gae_lambda', 0.8, 1.)
+        'gamma': trial.suggest_loguniform('gamma', 0.9, 0.9997),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 5e-6, 0.003), 
+        'ent_coef': trial.suggest_loguniform('ent_coef', 1e-8, 0.01), 
+        'clip_range': trial.suggest_uniform('clip_range', 0.1, 0.1), # try 0.2 and 0.3 as well
+        'n_epochs': int(trial.suggest_loguniform('n_epochs', 3, 30)),
+        'gae_lambda': trial.suggest_uniform('gae_lambda', 0.9, 1.0),
+        'batch_size': int(trial.suggest_uniform('batch_size', 4, 4096)),
+        'vf_coef': trial.suggest_uniform('v_coef', 0.5, 1.0)
+        #'max_grad_norm': trial.suggest_uniform('max_grad_norm', 0.9, 1.0)	
     }
 
 
 def optimize_agent(trial):
 	
     global step
+    global study
     
     env_count = 1 # Number of environments to run in parallel (keep at 1 for now)
-    timesteps = 100 # Timesteps that the model will train on before getting assessed and tuned
-    episodes_per_trial = 1 # Number of episode attempts (runs) per trial
+    timesteps = 1000 # Timesteps that the model will train on before getting assessed and tuned
+    episodes_per_trial = 6 # Number of episode attempts (runs) per trial
     
     ### Train the model and optimize it.
     
@@ -53,6 +62,8 @@ def optimize_agent(trial):
     
     env = make_vec_env(RocketLeagueInterface, env_kwargs={'run_id' : run_id, 'launch_args': ['render:=false', 'plot_log:=false']},
             n_envs= env_count , vec_env_cls=SubprocVecEnv) # Creates the env
+            
+    env = VecCheckNan(env, raise_exception=True)
     
     model = PPO("MlpPolicy", env, **model_params) # Creates the PPO network model based on the env and the provided hyperparameters.
     
@@ -64,12 +75,22 @@ def optimize_agent(trial):
 
     obs = env.reset()
     while n_episodes < episodes_per_trial:
+    
+        if np.isnan(obs).any():
+            print("Best Params:" + str(study.best_params))
+            #print("\n\n OBS IS NAN\n\n")
+            sys.exit("OBS is NAN")
+  
         action, _ = model.predict(obs)
-        
-        if not obs.all() == obs.all():
-        	print("\nOBS BAD\n")
-        
+               
+        if np.isnan(action).any():
+            print("Best Params:" + str(study.best_params))
+            #print("\n\n ACTION IS NAN\n\n")
+            sys.exit("ACTION IS NAN")
+               
         obs, reward, done, _ = env.step(action)
+        
+        
         
         reward_sum += reward
 
@@ -98,8 +119,7 @@ def optimize_agent(trial):
 
 
 if __name__ == '__main__':      # this is required due to forking processes
-    
-    study = optuna.create_study()
+    #study = optuna.create_study()
     
     try:
         study.optimize(optimize_agent, n_trials=10000, n_jobs=24)
