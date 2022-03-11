827a5587b4c1f3f4a6e4c7c09b7c829d93cc91c6
James Baxter <baxterjr@outlook.com>
2022-03-11 11:14:23 -0500

    updated script

--- rktl_control/scripts/analyze_step.m
+++ rktl_control/scripts/analyze_step.m
@@ -5,6 +5,7 @@
 %   for bag in *.bag; do
 %   rostopic echo -b $bag -p effort/throttle > "$bag"_effort.csv
 %   rostopic echo -b $bag -p odom/twist/twist/linear/x > "$bag"_velocity.csv
+%   rostopic echo -b $bag -p pose_sync/pose/pose/position > "$bag"_position.csv
 %   done
 
 close all
@@ -20,61 +21,31 @@ for i = 1:numel(files)
         continue
     end
 
+    % read in data
     effort = readtable(strcat(file.folder, '/', file.name, '_effort.csv'));
     velocity = readtable(strcat(file.folder, '/', file.name, '_velocity.csv'));
-
-    % ROS toolbox commands:
-    % may work on Linux, don't work on Windows due to custom msgs
-    % would remove need for bash command above, and allow direct access to bag file
-    %   bag = rosbag(strcat(file.folder, '/', file.name));
-    %   effort = timeseries(select(bag, 'Topic', 'effort'), 'throttle');
-    %   velocity = timeseries(select(bag, 'Topic', 'odom'), 'twist.twist.linear.x');
+    position = readtable(strcat(file.folder, '/', file.name, '_position.csv'));
 
     % convert time from ns since epoch to seconds since start of bag
     t0 = min(effort.x_time(1), velocity.x_time(1));
     effort.x_time = (effort.x_time - t0) * 10^-9;
     velocity.x_time = (velocity.x_time - t0) * 10^-9;
+    position.x_time = (position.x_time - t0) * 10^-9;
+
+    % manually calculate velocity from position to compare to filter output
+    xy = [position.field_x position.field_y];
+    delta_xy = [0 0; xy(2:end, :) - xy(1:end-1, :)];
+    dt = position.x_time(2) - position.x_time(1);
+    v_raw = sqrt(sum(delta_xy.^2 / dt, 2));
+    v_smooth = smoothdata(v_raw);
 
     figure
     plot(effort.x_time, effort.field), hold
     plot(velocity.x_time, velocity.field)
+    plot(position.x_time, v_raw)
+    plot(position.x_time, v_smooth)
     xlabel("Time, seconds")
-    ylabel("effort | velocity, m/s")
+    ylabel("Effort | Velocity, m/s")
+    legend("effor", "filter velocity", "raw velocity", "smoothed velocity")
     title(file.name, 'Interpreter', 'none')
-    legend('effort', 'velocity')
 end
-
-% % Optionally, overwrite time with more precise values
-% % poses.Time = poses.Data(:, 1) + poses.Data(:, 1).*10^-9;
-% 
-% % Isolate position (x, y, z)
-% raw_poses.Data = raw_poses.Data(:, 4:6);
-% poses.Data = poses.Data(:, 4:6);
-% 
-% % Calculate velocity
-% delta_xyz = poses.Data - [0 0 0; poses.Data(1:end-1, :)];
-% delta_t = poses.Time - [0; poses.Time(1:end-1, :)];
-% v_xyz = delta_xyz ./ delta_t;
-% v = sqrt(sum(v_xyz.^2, 2));
-% 
-% velocities = timeseries(smoothdata(v), poses.Time);
-% 
-% % Calculate effort
-% efforts = timeseries(select(bag, "Topic", "control_effort/throttle"));
-% 
-% t0 = min(poses.Time(1), efforts.Time(1));
-% raw_poses.Time = raw_poses.Time - t0;
-% poses.Time = poses.Time - t0;
-% velocities.Time = velocities.Time - t0;
-% efforts.Time = efforts.Time - t0;
-% 
-% hold on
-% yyaxis left
-% plot(velocities);
-% yyaxis right
-% plot(efforts);
-% 
-% figure
-% hold on
-% plot(raw_poses);
-% plot(poses);
