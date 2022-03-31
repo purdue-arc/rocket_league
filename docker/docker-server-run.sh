cbbaaad8e605b357224203f156e760872b6b954b
Jake Tockerman <jaketockerman@gmail.com>
2022-03-31 22:29:07 +0000

    remote x11 not using net=host via xauth voodoo

--- docker/docker-server-run.sh
+++ docker/docker-server-run.sh
@@ -18,21 +18,30 @@ fi
 
 $WS_DIR/src/rocket_league/docker/docker-build.sh
 
+#XAUTH VOODOO
+rm -rf /home/$USER/.docker.tmp
+mkdir /home/$USER/.docker.tmp
+cp /home/$USER/.Xauthority /home/$USER/.docker.tmp/
+X11PORT=`echo $DISPLAY | sed 's/^[^:]*:\([^\.]\+\).*/\1/'`
+MAGIC_COOKIE=`xauth list $DISPLAY | awk '{print $3}'`
+xauth -f /home/$USER/.docker.tmp/.Xauthority add 172.17.0.1:$X11PORT . $MAGIC_COOKIE
+DISPLAY=`echo $DISPLAY | sed 's/^[^:]*\(.*\)/172.17.0.1\1/'`
+
 docker run --rm \
     $TTY_OPTS \
     -e USER \
     -e DISPLAY \
+    -e XAUTHORITY=`echo /home/$USER/.docker.tmp/.Xauthority` \
     -e NVIDIA_DRIVER_CAPABILITIES=all \
-    -v $XAUTHORITY:/home/$USER/.Xauthority:ro \
+    -v /home/$USER/.docker.tmp/:$HOME/.docker.tmp:ro \
     -v $WS_DIR:/home/$USER/catkin_ws \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     `[ -f ~/.gitconfig ] && echo "-v $HOME/.gitconfig:/home/$USER/.gitconfig:ro"` \
-    -v $HOME/.Xauthority:/home/$USER/.Xauthority \
     -v ~/.ssh:/home/$USER/.ssh:ro \
-    --net host \
     --name $USER-$CONTAINER_NAME \
     $@ \
     $REPO_NAME:local-$USER \
+    ${DOCKER_CMD:+/bin/zsh -c "ln -s $HOME/.docker.tmp/.Xauthority $HOME/.Xauthority"} \
     ${DOCKER_CMD:+/bin/zsh -c "$DOCKER_CMD"}
 
-docker rmi -f $REPO_NAME:local-$USER
+
