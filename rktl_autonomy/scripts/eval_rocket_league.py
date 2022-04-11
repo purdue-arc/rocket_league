4ab57661055c58496c96965d2cc1e8756bb9fef1
James Baxter <baxterjr@outlook.com>
2022-04-11 10:30:00 -0400

    newlines

--- rktl_autonomy/scripts/eval_rocket_league.py
+++ rktl_autonomy/scripts/eval_rocket_league.py
@@ -31,14 +31,14 @@ if __name__ == '__main__':      # this is required due to forking processes
         model_dir = expanduser(f'~/catkin_ws/data/rocket_league/{model_run_id}')
 
         with open(f'{model_dir}/eval_log.txt', 'w') as logfile:
-            logfile.write(f"Training Episodes, mean reward, std dev reward")
+            logfile.write(f"Training Episodes, mean reward, std dev reward\n")
 
             for weight in glob(f'{model_dir}/rl_model_*_steps.zip'):
                 model = PPO.load(weight.replace('.zip', ''))
                 mu, sigma = evaluate_policy(model, env, n_eval_episodes=1)
 
                 episodes = weight.replace(f'{model_dir}/rl_model_', '').replace('_steps.zip', '')
-                logfile.write(f"{episodes}\t{mu:.3f}\t{sigma:.3f}")
+                logfile.write(f"{episodes}\t{mu:.3f}\t{sigma:.3f}\n")
                 logfile.flush()
 
     env.close() # this must be done to clean up other processes
