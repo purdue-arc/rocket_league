f906b926c2328f73013a1712fdde53a644a5141f
ajernandes <aj.ernandes01@gmail.com>
2022-04-13 12:41:19 -0400

    split js into dedicated file

---
+++ rktl_game/index.js
@@ -0,0 +1,88 @@
+// Connecting to ROS
+// -----------------
+
+var ros = new ROSLIB.Ros({
+    url: 'ws://172.17.0.2:9090'
+});
+
+ros.on('connection', function () {
+    console.log('Connected to websocket server.');
+});
+
+ros.on('error', function (error) {
+    console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function () {
+    console.log('Connection to websocket server closed.');
+});
+
+// Setting cars to disabled
+// ------------------
+
+// var carEnable = new ROSLIB.Topic({
+//     ros: ros,
+//     name: '/cars/enable',
+//     messageType: 'std_msgs/Bool'
+// });
+
+// var carBool = new ROSLIB.Message({
+//     data: false
+// });
+
+// carEnable.publish(carBool);
+
+// Subscribing to a Topic
+// ----------------------
+// Subscribe to the score keeper node
+var listener = new ROSLIB.Topic({
+    ros: ros,
+    name: '/score',
+    messageType: 'rktl_msgs/Score'
+});
+
+// updating the score on a timer
+const interval = setInterval(function () {
+    listener.subscribe(function (message) {
+document.getElementById('blue-score').innerHTML = message.blue;
+document.getElementById('orange-score').innerHTML = message.orange;
+    });
+}, 1000);
+
+// // Calling a service
+// // -----------------
+
+// var addTwoIntsClient = new ROSLIB.Service({
+//     ros: ros,
+//     name: '/add_two_ints',
+//     serviceType: 'rospy_tutorials/AddTwoInts'
+// });
+
+// var request = new ROSLIB.ServiceRequest({
+//     a: 1,
+//     b: 2
+// });
+
+// addTwoIntsClient.callService(request, function (result) {
+//     console.log('Result for service call on ' +
+// addTwoIntsClient.name +
+// ': ' +
+// result.sum);
+// });
+
+// Getting and setting a param value
+// ---------------------------------
+
+ros.getParams(function (params) {
+    console.log(params);
+});
+
+var maxVelX = new ROSLIB.Param({
+    ros: ros,
+    name: 'max_vel_y'
+});
+
+maxVelX.set(0.8);
+maxVelX.get(function (value) {
+    console.log('MAX VAL: ' + value);
+});
