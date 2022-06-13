5a51127a6a23c2288f47b7c064f2f622bf1a5e66
Alexiy Buynitsky <abuynits@gmail.com>
2022-06-12 19:56:28 -0700

    train_rocket_league.py script: added comments

--- rktl_autonomy/scripts/train_rocket_league.py
+++ rktl_autonomy/scripts/train_rocket_league.py
@@ -15,13 +15,23 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 from os.path import expanduser
 import uuid
 
-if __name__ == '__main__':      # this is required due to forking processes
+"""
+overview:
+All run env share a run_id
+This is where you specify launch argos: add to the env_kwargs
+Set the model that you will do to use for the training
+Log the training process for the specific run id
+Specify the stems and let the model learn wit these specific steps
+useful resource for stable_baselines3: https://stable-baselines3.readthedocs.io/_/downloads/en/master/pdf/
+"""
+
+if __name__ == '__main__':  # this is required due to forking processes
     run_id = str(uuid.uuid4())  # ALL running environments must share this
     print(f"RUN ID: {run_id}")
 
     # to pass launch args, add to env_kwargs: 'launch_args': ['render:=false', 'plot_log:=true']
-    env = make_vec_env(RocketLeagueInterface, env_kwargs={'run_id':run_id},
-            n_envs=24, vec_env_cls=SubprocVecEnv)
+    env = make_vec_env(RocketLeagueInterface, env_kwargs={'run_id': run_id},
+                       n_envs=24, vec_env_cls=SubprocVecEnv)
 
     model = PPO("MlpPolicy", env)
 
@@ -31,16 +41,16 @@ if __name__ == '__main__':      # this is required due to forking processes
     model.set_logger(logger)
 
     # log model weights
-    freq = 20833 # save 20 times
+    freq = 20833  # save 20 times
     # freq = steps / (n_saves * n_envs)
     callback = CheckpointCallback(save_freq=freq, save_path=log_dir)
 
     # run training
-    steps = 240000000 # 240M (10M sequential)
+    steps = 240000000  # 240M (10M sequential)
     print(f"training on {steps} steps")
     model.learn(total_timesteps=steps, callback=callback)
 
     # save final weights
     print("done training")
     model.save(log_dir + "/final_weights")
-    env.close() # this must be done to clean up other processes
+    env.close()  # this must be done to clean up other processes
