bd2cb7f0475cffb504b0329cdafc5df659dba6ba
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2022-10-06 19:57:52 -0400

    Fix file loading

--- rktl_autonomy/scripts/modular_train.py
+++ rktl_autonomy/scripts/modular_train.py
@@ -2,13 +2,17 @@ from train_rocket_league import train
 import yaml
 from rktl_autonomy import EnvCounter
 import sys
+import os
 from pathlib import Path
 
 if __name__ == '__main__':
     numEnvsAllowed = 24
     if len(sys.argv) == 2:
         numEnvsAllowed = int(sys.argv[1])
-    file = yaml.load(open(Path(__file__).parent.parent / "config" / "rocket_league.yaml"), Loader=yaml.FullLoader)
+    configFolder = os.path.join(os.path.join(os.path.join(Path(__file__), os.pardir), os.pardir), Path("config"))
+    configFile = Path(os.path.abspath(os.path.join(configFolder, "rocket_league.yaml")))
+    print(os.path.abspath(configFile))
+    file = yaml.load(open(configFile), Loader=yaml.FullLoader)
     numGroups = len(file["reward"]["win"])
     for i in range(numGroups):
-        train(n_envs=numEnvsAllowed / numGroups, env_counter=EnvCounter())
+        train(n_envs=int(numEnvsAllowed / numGroups), env_counter=EnvCounter())
