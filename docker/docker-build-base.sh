4ffa5d65e8e91238660354f619536b19a6918119
Daniel Gerblick <daniel.gerblick@gmail.com>
2023-02-25 15:04:01 -0500

    Added websockets to dockerfile

--- docker/docker-build-base.sh
+++ docker/docker-build-base.sh
@@ -3,7 +3,7 @@
 DOCKER_DIR=$(realpath $(dirname $0))
 REPO_NAME="purduearc/rocket-league"
 
-docker buildx build \
+docker build \
     --tag $REPO_NAME \
     --cache-from=type=registry,ref=${REPO_NAME}:cache \
     $@ \
