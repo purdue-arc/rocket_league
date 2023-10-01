187fb06b46543fc312504956de3188a7bc4eac5f
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2023-10-01 18:30:20 +0000

    Create ROS 2 rktl_sim, backup ROS 1

--- rktl_sim/setup.py
+++ rktl_sim/setup.py
@@ -1,11 +1,25 @@
-#!/usr/bin/env python3
+from setuptools import find_packages, setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'rktl_sim'
 
-setup_args = generate_distutils_setup(
-    packages=['simulator', 'visualizer'],
-    package_dir={'': 'src'}
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jcrm1',
+    maintainer_email='jcrm1@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
-
-setup(**setup_args)
