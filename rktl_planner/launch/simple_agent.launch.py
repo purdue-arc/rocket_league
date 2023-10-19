d880c4700ab5b8b2660ec18c2771733719074d13
sixfootsix50 <sixfootsix50@gmail.com>
2023-10-19 00:47:26 +0000

    Updated simple_agent.launch.py

--- rktl_planner/launch/simple_agent.launch.py
+++ rktl_planner/launch/simple_agent.launch.py
@@ -3,7 +3,6 @@ import sys
 
 import launch
 import launch_ros.actions
-#WIP
 
 def generate_launch_description():
     ld = launch.LaunchDescription([
@@ -18,22 +17,44 @@ def generate_launch_description():
         launch.actions.GroupAction(
             actions=[
                 launch_ros.actions.PushRosNamespace("agents/" + launch.substitutions.LaunchConfiguration("agent_name")),
+                launch_ros.actions.Node(
+                    package="rktl_planner",
+                    executable="path_follower",
+                    name="path_follower",
+                    output="screen",
+                    parameters=[
+                        {
+                            launch.substitutions.PathJoinSubstitution(launch_ros.substitutions.FindPackageShare('rktl_planner'), '/config/path_follower.yaml')
+                        },
+                        {
+                            "car_name": launch.substitutions.LaunchConfiguration("car_name")
+                        }
+                    ]
+                ),
+                launch_ros.actions.Node(
+                    package="rktl_planner",
+                    executable="bezier_path_server",
+                    name="bezier_path_server",
+                    output="screen"
+                ),
+                launch_ros.actions.Node(
+                    package="rktl_planner",
+                    executable="path_planner",
+                    name="path_planner",
+                    output="screen",
+                    parameters=[
+                        {
+                            launch.substitutions.PathJoinSubstitution(launch_ros.substitutions.FindPackageShare('rktl_planner'), '/config/path_planner.yaml')
+                        },
+                        {
+                            "car_name": launch.substitutions.LaunchConfiguration("car_name")
+                        }
+                    ]
+                )
             ]
-            # launch_ros.actions.Node(
-            #     package="rktl_planner"
-            #     name="path_follower"
-            #     executable="path_follower"
-            #     output="screen"
-            #     parameters=[
-            #         {
-            #             "car_name": launch.substitutions.LaunchConfiguration("car_name")
-            #         }
-            #     ]
-            # )
         )
     ])
     return ld
-
 
 if __name__ == '__main__':
     generate_launch_description()
