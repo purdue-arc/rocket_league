b873503cc1df3634318b6faaa30c7c2ac59a08dc
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2023-11-12 19:55:54 +0000

    Revert "Revert "partial migrate rktl_game""
    This reverts commit 4502794d5cda4ce49a889dcd9803b7dd6701db10.
    Reverted the wrong commit

---
+++ rktl_game/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'rktl_game'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jcrm1',
+    maintainer_email='52137472+jcrm1@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
