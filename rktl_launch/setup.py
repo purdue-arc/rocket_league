d7ebdf165301489850df54ede0500e971b738795
sixfootsix50 <sixfootsix50@gmail.com>
2023-11-16 01:47:54 +0000

    Updated rktl_launch file structure

--- rktl_launch/setup.py
+++ rktl_launch/setup.py
@@ -1,18 +1,15 @@
 from setuptools import find_packages, setup
-import os
-import glob
 
 package_name = 'rktl_launch'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob.glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
