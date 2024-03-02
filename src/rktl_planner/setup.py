056114da96f5087ed8d3619a17fa1ed2955a00bd
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2024-03-02 20:39:05 +0000

    copy nodes on install

--- src/rktl_planner/setup.py
+++ src/rktl_planner/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         (os.path.join('lib', package_name), glob(os.path.join(package_name, '*.py'))),
+        (os.path.join('lib', package_name), glob(os.path.join(package_name, 'nodes', '*.py'))),
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
